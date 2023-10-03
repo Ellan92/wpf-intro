fa8f15dd047e61191a18a2e6ba3e6904654ed952
Ellan92 <elle9121@hotmail.se>
2023-10-03 14:21:09 +0200

    Initial commit

---
+++ WPFIntro/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace WPFIntro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // läs inputrutorna
+            string inputName = txtName.Text;
+            string inputEmail = txtEmail.Text;
+
+            // Skapa en interpolerad sträng med all information
+            string personString = $"{inputName} - Email: {inputEmail}";
+
+            // Lägg till den interpolerade strängen i listan
+            lstPeople.Items.Add(personString);
+
+            // Rensa inputrutorna
+            txtName.Text = "";
+            txtEmail.Clear();
+        }
+    }
+}
